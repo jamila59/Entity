13712734098841e56dd8b4bf28c8ba6bffa122c4
Jamila <Jama@Mikhailas-MacBook-Pro.local>
2020-03-25 15:48:49 -0700

    utilize the Entity Framework to configure Resful web-services and save objects to database records.

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Entity.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    private readonly ToDoListContext _db;
+
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Item> model = _db.Items.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Item item)
+    {
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,8 @@
+namespace Entity.Models
+{
+  public class Item
+  {
+    public int ItemId { get; set; }
+    public string Description { get; set; }
+  }
+}
